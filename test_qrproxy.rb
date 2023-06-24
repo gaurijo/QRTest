d1549ccf9482b1cbcb586eb01d1d8f518db430e9
Gauri Joshi <gauri.joshi@planetargon.com>
2023-06-23 17:15:52 -0700

    checks single color radio button is default enabled

--- test_qrproxy.rb
+++ test_qrproxy.rb
@@ -6,9 +6,14 @@ class QRProxyTest < MiniTest::Test
   def setup 
     @browser = Selenium::WebDriver.for :chrome 
   end
-
+  
   def test_qrproxy
     @browser.navigate.to "https://www.qrcode-monkey.com/#url"
     assert(@browser.title.include? "QRCode Monkey - The free QR Code Generator")
+
+    ## check single color radio button is enabled by default
+    @single_color = @browser.find_element(:name => "colorMode", :value => "single")
+    assert(@single_color.enabled?)
   end
+
 end
