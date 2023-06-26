2ee9fa3bef8785ba30c4521a5f1cb7f1b8cb4682
Gauri Joshi <gauri.joshi@planetargon.com>
2023-06-26 16:25:16 -0700

    Tests a user clicking on a specific design pattern for the body shape of the QR code

--- test_runner.rb
+++ test_runner.rb
@@ -35,38 +35,50 @@ class QRProxyTest < MiniTest::Test
     @browser.execute_script("arguments[0].click();", @generate)
     assert(@generate.enabled? == false)
 
-    #finds the preview for no logo
+    ## finds the preview for no logo
     @no_logo = @browser.find_elements(:xpath, "//div[@class='placeholder']//span[text()='No Logo']")
     assert(@no_logo.any?)
     #todo: user downloads QR code as PNG
 
     ## user quits browser 
-    # @browser.quit 
+    # @browser.quit
 
-  ## Scenario 2: User customizes their inputs before generating qr code
-    ## check when add_logo pane/row is clicked on, it turns the pane editing view to active"
+
+  ##Scenario 2: User customizes their inputs before generating qr code
+    ## check when add_logo pane/row is clicked on, it turns the pane editing view to active
     @add_logo = @browser.find_element(:class, "pane")
     @browser.execute_script("arguments[0].click();", @add_logo)
     assert(@add_logo[:class] == "pane active")
 
-    ##assert that the class attribute for the logo element is NOT active at first
+    ## assert that the class attribute for the logo element is NOT active at first
     @logo_element = @browser.find_element(:css, "div.shape.ng-scope")
     @class = @logo_element.attribute("class")
     assert(!@class.include?("active"))
     
     ## after clicking on a pre-selected logo, check that the logo element class attribute turns to active
+    @browser.execute_script("arguments[0].click();", @logo_element)
     @updated_class = @logo_element.attribute("class")
     assert(@updated_class.include?("active"))
+
+    #todo: test change design pattern
+    @design_pattern = @browser.find_element(:css, "div.shape.ng-scope i.sprite.sprite-body.sprite-square")
+    @parent_div = @design_pattern.find_element(:xpath, "./ancestor::div[contains(@class, 'shape')][contains(@class, 'active')]")
+
+    @browser.execute_script("arguments[0].click();", @design_pattern)
+
+    @updated_design = @parent_div.attribute("class")
+
+    assert(@updated_design.include?("active"))    
+    #todo: test customize colors 
   end
 end
   
-  #todo: change design pattern
-  #todo: customize colors 
   # @color_picker = @browser.find_element(:css, "background-color")
   # @new_color = 'red'
   # actual_color = @color_picker.attribute("background-color")
   # expected_color = 'rgb(255, 0, 0)'
   # assert(actual_color == expected_color)
-  ## Scenario 3: Error Handling -- if a user tries to implement something they cannot or should not they will get an alert
+
+  ##Scenario 3: Error Handling -- if a user tries to implement something they cannot or should not they will get an alert
   ## for foreground color, if the color is too light (>= 160) an alert pops up 'we recommend to make your color darker'
     
