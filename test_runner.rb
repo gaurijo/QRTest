5fb8772f953e1eea82f46ba61fae9831be243fac
Gauri Joshi <gauri.joshi@planetargon.com>
2023-06-26 14:30:34 -0700

    Tests that the logo preview displays 'No Logo' by default

--- test_runner.rb
+++ test_runner.rb
@@ -5,7 +5,7 @@ require 'webdrivers'
 class QRProxyTest < MiniTest::Test 
   def setup 
     @browser = Selenium::WebDriver.for :chrome 
-    @wait = Selenium::WebDriver::Wait.new(:timeout => 15) #not sure if I need to keep this line
+    @wait = Selenium::WebDriver::Wait.new(:timeout => 10) #not sure if I need to keep this line
   end
   
   ##Scenario: User navigates to the website, and does not change anything, website should generate a default QR code:
@@ -35,28 +35,35 @@ class QRProxyTest < MiniTest::Test
     @browser.execute_script("arguments[0].click();", @generate)
     assert(@generate.enabled? == false)
 
-    ## user downloads QR code as PNG
-    ## user quits browser 
-  end
+    #todo: user downloads QR code as PNG
 
-  ## Scenario Two: User customizes their inputs before generating qr code
-  def test2_qrproxy
+    ## user quits browser 
+    # @browser.quit 
+
+  ## Scenario 2: User customizes their inputs before generating qr code
+  #todo: add a logo or upload image logo
     ## check when add_logo pane/row is clicked on, it turns the pane editing view to active"
     @add_logo = @browser.find_element(:class, "pane")
     @browser.execute_script("arguments[0].click();", @add_logo)
     assert(@add_logo[:class] == "pane active")
 
-    ## customize URL input
-    @input_url = @browser.find_element(:name, "qrcodeUrl")
-    @input_url.send_keys('https://www.myqrcode.com')
+    #finds the preview for no logo
+    @no_logo = @browser.find_elements(:xpath, "//div[@class='placeholder']//span[text()='No Logo']")
+    assert(@no_logo.any?)
 
-    expected_result = 'https://www.myqrcode.com'
-    actual_result = @input_url.attribute('value')
-    assert(expected_result == actual_result)
-
-    ## customize colors 
-
+    # @logo_preview = @new_logo.find_element(:class, "placeholder")
+    # @browser.execute_script("arguments[0].click();", @new_logo)
+    # assert(@logo_preview[:class] == "placeholder ng-hide")
+    #todo: change design pattern
   end
+  
+  #todo: customize colors 
+  # @color_picker = @browser.find_element(:css, "background-color")
+  # @new_color = 'red'
+  # actual_color = @color_picker.attribute("background-color")
+  # expected_color = 'rgb(255, 0, 0)'
+  # assert(actual_color == expected_color)
+  ## Scenario 3: Error Handling -- if a user tries to implement something they cannot or should not they will get an alert
+  ## for foreground color, if the color is too light (>= 160) an alert pops up 'we recommend to make your color darker'
 end
     
-    ## for foreground color, if the color is too light (>= 160) an alert pops up 'we recommend to make your color darker'
