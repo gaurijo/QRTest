709860395a9f102424256978bba5f4b581f53668
Gauri Joshi <gauri.joshi@planetargon.com>
2023-06-27 16:36:44 -0700

    Clean up code comments and structure

--- test_runner.rb
+++ test_runner.rb
@@ -16,33 +16,33 @@ class QRProxyTest < MiniTest::Test
     assert(@browser.title.include? "QRCode Monkey - The free QR Code Generator")
 
     ## check single color radio button is enabled by default
-    @single_color = @browser.find_element(:name => "colorMode", :value => "single")
-    @gradient = @browser.find_element(:name => "colorMode", :value => "gradient")
-    assert(@single_color.enabled?)
+    single_color = @browser.find_element(:name => "colorMode", :value => "single")
+    gradient = @browser.find_element(:name => "colorMode", :value => "gradient")
+    assert(single_color.enabled?)
 
     ## check that gradient radio button is selected when clicked on
-    @browser.execute_script("arguments[0].click();", @gradient)
-    assert(@gradient.selected?)
+    @browser.execute_script("arguments[0].click();", gradient)
+    assert(gradient.selected?)
 
     ## check that custom eye color checkbox is NOT selected by default
-    @cb = @browser.find_element(:css, 'input[ng-model="customEyeColor"]')
-    assert(@cb.selected? == false)
+    cb = @browser.find_element(:css, 'input[ng-model="customEyeColor"]')
+    assert(cb.selected? == false)
 
     ## check that create qr code button is enabled by default
-    @generate = @browser.find_element(:id, "button-create-qr-code")
-    assert(@generate.enabled?)
+    generate = @browser.find_element(:id, "button-create-qr-code")
+    assert(generate.enabled?)
 
     ## finds the preview for no logo
-    @no_logo = @browser.find_elements(:xpath, "//div[@class='placeholder']//span[text()='No Logo']")
-    assert(@no_logo.any?)
+    no_logo = @browser.find_elements(:xpath, "//div[@class='placeholder']//span[text()='No Logo']")
+    assert(no_logo.any?)
 
     ## create the qr code; check that create qr code button is disabled once a user clicks on it
-    @browser.execute_script("arguments[0].click();", @generate)
-    assert(@generate.enabled? == false)
+    @browser.execute_script("arguments[0].click();", generate)
+    assert(generate.enabled? == false)
 
     ## user downloads their QR code as default png file
-    @png = @browser.find_element(:id, "button-download-qr-code-png")
-    @browser.execute_script("arguments[0].click();", @png)
+    png = @browser.find_element(:id, "button-download-qr-code-png")
+    @browser.execute_script("arguments[0].click();", png)
 
     assert(@browser.page_source.include?("download('png')"), "Download action failed")
 
@@ -51,37 +51,37 @@ class QRProxyTest < MiniTest::Test
 
   ##Scenario 2: User customizes some inputs before generating qr code
     ## check when add_logo pane/row is clicked on, it turns the pane editing view to active
-    @add_logo = @browser.find_element(:class, "pane")
-    @browser.execute_script("arguments[0].click();", @add_logo)
-    assert(@add_logo[:class] == "pane active")
+    add_logo = @browser.find_element(:class, "pane")
+    @browser.execute_script("arguments[0].click();", add_logo)
+    assert(add_logo[:class] == "pane active")
 
     ## assert that the class attribute for the logo element is NOT active at first
-    @logo_element = @browser.find_element(:css, "div.shape.ng-scope")
-    @class = @logo_element.attribute("class")
+    logo_element = @browser.find_element(:css, "div.shape.ng-scope")
+    @class = logo_element.attribute("class")
     assert(!@class.include?("active"))
     
     ## after clicking on a pre-selected logo, check that the logo element class attribute turns to active
-    @browser.execute_script("arguments[0].click();", @logo_element)
-    @updated_class = @logo_element.attribute("class")
-    assert(@updated_class.include?("active"))
+    @browser.execute_script("arguments[0].click();", logo_element)
+    updated_class = logo_element.attribute("class")
+    assert(updated_class.include?("active"))
 
     ## after clicking on a body shape, check that the class for that shape element is turned to active
-    @design_pattern = @browser.find_element(:css, "div.shape.ng-scope i.sprite.sprite-body.sprite-square")
-    @parent_div = @design_pattern.find_element(:xpath, "./ancestor::div[contains(@class, 'shape')][contains(@class, 'active')]")
+    design_pattern = @browser.find_element(:css, "div.shape.ng-scope i.sprite.sprite-body.sprite-square")
+    parent_div = design_pattern.find_element(:xpath, "./ancestor::div[contains(@class, 'shape')][contains(@class, 'active')]")
 
-    @browser.execute_script("arguments[0].click();", @design_pattern)
+    @browser.execute_script("arguments[0].click();", design_pattern)
 
-    @updated_design = @parent_div.attribute("class")
+    updated_design = parent_div.attribute("class")
 
-    assert(@updated_design.include?("active"))
+    assert(updated_design.include?("active"))
 
     #todo: test eye frame shape if you have time 
 
 
   ##Scenario 3: Error Handling -- if a user tries to implement something they cannot or should not they will get an alert
     #by default alert is hidden
-    @alert = @browser.find_element(:css, "div.fade-animation.alert.alert-warning.ng-hide")
-    assert(!@alert.displayed?)
+    alert = @browser.find_element(:css, "div.fade-animation.alert.alert-warning.ng-hide")
+    assert(!alert.displayed?)
 
     #access the element to pick a color that's too light
     input_color = "rgb(246, 234, 234)"
@@ -89,12 +89,12 @@ class QRProxyTest < MiniTest::Test
     @browser.execute_script("arguments[0].setAttribute('style', 'background-color: #{input_color}; opacity: 1')", input_color_element)
     
     #access the color error text that is not hidden
-    @visible_alert = @browser.find_element(:css, "div.fade-animation.alert.alert-warning")
+    visible_alert = @browser.find_element(:css, "div.fade-animation.alert.alert-warning")
 
     #removes any leading/trailing whitespace to access the actual message of the alert
-    @visible_text = @visible_alert.attribute('innerText').strip
+    visible_text = visible_alert.attribute('innerText').strip
 
-    assert(@visible_text == "We recommend to make your color darker")
+    assert(visible_text == "We recommend to make your color darker")
   end
 end
 
